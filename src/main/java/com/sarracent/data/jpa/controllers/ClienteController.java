08bce7f3847d9deebe3b67128c499d048d4eb3e1
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-14 00:58:49 -0300

    114. Creando controlador con la acción handler listar

---
+++ src/main/java/com/sarracent/data/jpa/controllers/ClienteController.java
@@ -0,0 +1,21 @@
+package com.sarracent.data.jpa.controllers;
+
+import com.sarracent.data.jpa.models.dao.IClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClienteController {
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", "Listado de clientes");
+        model.addAttribute("clientes", clienteDao.findAll());
+        return "listar";
+    }
+}
