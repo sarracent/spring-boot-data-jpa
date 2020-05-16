9167c605f2daf68ea34fb62503174d76961c8508
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-16 18:21:26 -0300

    125. Agregando editar

--- src/main/java/com/sarracent/data/jpa/controllers/ClienteController.java
+++ src/main/java/com/sarracent/data/jpa/controllers/ClienteController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 
     @Autowired
@@ -33,13 +34,29 @@ public class ClienteController {
         return "form";
     }
 
+    @RequestMapping(value="/form/{id}")
+    public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+
+        Cliente cliente = null;
+
+        if(id > 0) {
+            cliente = clienteDao.findOne(id);
+        } else {
+            return "redirect:/listar";
+        }
+        model.put("cliente", cliente);
+        model.put("titulo", "Editar Cliente");
+        return "form";
+    }
+
     @PostMapping("/form")
-    public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+    public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
         clienteDao.save(cliente);
+        status.setComplete();
         return "redirect:listar";
     }
 }
